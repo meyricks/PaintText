a5fc78345dcb25b9049231e8492189a791c22c58
meyricks <abrahamer122@gmail.com>
2022-10-24 14:36:22 -0400

    Agregar archivos de proyecto.

---
+++ PaintText/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PaintText
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Paint = new System.Windows.Forms.Button();
+            this.ETXT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Calligraphy", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(274, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(274, 59);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Menú";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Paint
+            // 
+            this.Paint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Paint.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Paint.Location = new System.Drawing.Point(331, 147);
+            this.Paint.Name = "Paint";
+            this.Paint.Size = new System.Drawing.Size(162, 61);
+            this.Paint.TabIndex = 1;
+            this.Paint.Text = "Paint";
+            this.Paint.UseVisualStyleBackColor = false;
+            this.Paint.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ETXT
+            // 
+            this.ETXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ETXT.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ETXT.Location = new System.Drawing.Point(298, 230);
+            this.ETXT.Name = "ETXT";
+            this.ETXT.Size = new System.Drawing.Size(230, 83);
+            this.ETXT.TabIndex = 2;
+            this.ETXT.Text = "Editor de texto";
+            this.ETXT.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(859, 458);
+            this.Controls.Add(this.ETXT);
+            this.Controls.Add(this.Paint);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button Paint;
+        private Button ETXT;
+    }
+}
